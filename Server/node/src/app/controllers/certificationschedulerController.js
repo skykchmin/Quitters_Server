25dcec95001b52c78ce03ce08233455012f4e6e8
skykchmin <skykchmin@naver.com>
2021-03-12 01:08:50 +0900

    [210312] 스케줄러 트랜잭션 추가
    [210312] 스케줄러 트랜잭션 추가

--- Server/node/src/app/controllers/certificationschedulerController.js
+++ Server/node/src/app/controllers/certificationschedulerController.js
@@ -5,9 +5,12 @@ const certificationschedulerDao = require('../dao/certificationschedulerDao');
 const { constants } = require('buffer');
 
 exports.updateChallengeParticipation = async function (req, res) {
-   
+    
+    const connection = await pool.getConnection(); // 트랜잭션 시작
+
         try {
             
+            await connection.beginTransaction();
             const getChallengeDeclarerObserverInfoRows = await certificationschedulerDao.getChallengeDeclarerObserverInfo(); // 친구의 챌린지 조회
 
             for(var i = 0; i < getChallengeDeclarerObserverInfoRows[0].length; i++){
@@ -22,13 +25,16 @@ exports.updateChallengeParticipation = async function (req, res) {
                 const updateChallengeCertificationInfoRows = await certificationschedulerDao.updateChallengeCertificationInfo(updateChallengeCertificationInfoParams);
             }
             
+            await connection.commit();
             console.log("감시자 자동 추가 이벤트 성공!");
             return true;
             
         } catch (err) {
-            // await connection.rollback(); // ROLLBACK
+            await connection.rollback(); // ROLLBACK
             console.log("감시자 자동 추가 이벤트 실패!");
             logger.error(`App - 챌린지 참여 Query error\n: ${err.message}`);
             return res.status(4000).send(`Error: ${err.message}`);
-        } 
+        } finally {
+            connection.release();
+        }
 };
